1eb39b32e5c9c66734c896292cb4cab99a1421b4
Telepathic <129508941+MrTelepathic@users.noreply.github.com>
2024-04-09 16:06:04 +0330

    Delete tp-scanner directory

--- tp-scanner/src/consts/index.ts
+++
@@ -1,7 +0,0 @@
-import cloudflareIpRanges from "./ip-ranges.json";
-
-import { rangeToIpArray } from "~/helpers/rangeToIpArray";
-
-export const allIps: string[] = cloudflareIpRanges.flatMap((range) =>
-  rangeToIpArray(range)
-);
